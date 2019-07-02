d8d4491cb3c786d42daf644aabe9c378bb589808
seolbluewings <tjfdudh@gmail.com>
2019-07-02 11:44:07 +0900

    revision

--- Bayesian/simulation_SCSAR_FE.R
+++ Bayesian/simulation_SCSAR_FE.R
@@ -2,24 +2,24 @@ rm(list=ls())
 set.seed(2081311133)
 library(mvtnorm)
 WC=read.csv("C://seolbluewings.github.io/Bayesian/WC_data.csv",header=T)
-y=WC[,4:5]; x=WC[,c(4,7:13,15,18)]
-Xg=list();yg=list()
+y=WC[,4:5]; x=WC[,c(4:5,7:13,15,18)]
+Xg=list();yg=list();zg=list()
 G=128
 
 ### allocate group ###
-for(g in 1:G){
-    yg[[g]]=as.vector(y[WC$group==g,2])
-    Xg[[g]]=as.data.frame(x[WC$group==g,-1])
-}
 ### calculate group size ###
 mg=c()
 for(g in 1:G){
-  mg[g]=length(yg[[g]])
+    yg[[g]]=as.vector(y[WC$group==g,2])
+    Xg[[g]]=as.data.frame(x[WC$group==g,-c(1:2)])
+    zg[[g]]=as.vector(scale(x[WC$group==g,2]))
+    mg[g]=length(yg[[g]])
 }
+
 Z_list=list()
 Cg1_list=list(); Cg2_list=list(); Cg3_list=list(); Cg4_list=list()
 for(g in 1:G){
-  z_mat=rnorm(mg[g],0,1)
+  z=zg[[g]]
   C_g1=matrix(0,ncol=mg[g],nrow=mg[g])
   C_g2=matrix(0,ncol=mg[g],nrow=mg[g])
   C_g3=matrix(0,ncol=mg[g],nrow=mg[g])
@@ -60,9 +60,8 @@ for(g in 1:G){
         C_g4[i,j]=0
       }
     }
-    
   }
-  Z_list[[g]]=z_mat
+  Z_list[[g]]=z
   Cg1_list[[g]]=C_g1; Cg2_list[[g]]=C_g2
   Cg3_list[[g]]=C_g3; Cg4_list[[g]]=C_g4
 }
@@ -80,11 +79,10 @@ beta7_list=list(); beta8_list=list(); beta9_list=list()
 beta10_list=list(); beta11_list=list(); beta12_list=list()
 beta13_list=list(); beta14_list=list(); beta15_list=list()
 beta16_list=list(); beta17_list=list(); beta18_list=list()
-beta19_list=list()
 gamma1_list=list(); gamma2_list=list(); gamma3_list=list()
 gamma4_list=list(); gamma5_list=list(); gamma6_list=list()
 lambda_list=list()
-sige_list=list();siga_list=list();sigez_list=list()
+sige_list=list();sigez_list=list()
 
 st_time=Sys.time()
 for(r in 1:R){
@@ -93,15 +91,15 @@ for(r in 1:R){
   
   ### assign parameter in prior distribution ###
   gamma0=rep(0,6); Gamma0=5*diag(1,6)
-  beta0=rep(0,2*dim(Xg[[1]])[2]+1); Beta0=5*diag(1,2*dim(Xg[[1]])[2]+1)
+  beta0=rep(0,2*dim(Xg[[1]])[2]); Beta0=5*diag(1,2*dim(Xg[[1]])[2])
   rho0=5; eta0=1; sig0=c(0,0)
+  alpha0=0; Alpha0=100
   
   Zlist=list() #save for Z
   gamma_mat=matrix(0,ncol=6,nrow=iter) #save for gamma
   lambda_mat=matrix(0,ncol=1,nrow=iter) #save for lambda
-  beta_mat=matrix(0,ncol=2*dim(Xg[[1]])[2]+1,nrow=iter) #save for beta
+  beta_mat=matrix(0,ncol=2*dim(Xg[[1]])[2],nrow=iter) #save for beta
   sige_mat=matrix(0,ncol=1,nrow=iter) #save for sigma_e^2
-  siga_mat=matrix(0,ncol=1,nrow=iter) #save for sigma_a^2
   sigez_mat=matrix(0,ncol=1,nrow=iter) #save for sigma_ez^2
   alpha_mat=matrix(0,nrow=iter,ncol=G) #save for alpha
   acc_rate1=matrix(0,nrow=iter,1) #acceptance rate gamma
@@ -111,13 +109,12 @@ for(r in 1:R){
   
   ### starting value of draw ###
   gamma_mat[1,]=c(-1,0.5,0.5,0.5,0.5,1);
-  sige_mat[1,]=1; sigez_mat[1,]=0.5; siga_mat[1,]=0.3
-  beta_mat[1,]=rep(0,2*dim(Xg[[1]])[2]+1)
+  sige_mat[1,]=1; sigez_mat[1,]=0.5; 
+  beta_mat[1,]=rep(0,2*dim(Xg[[1]])[2])
   
   cat(r,"repetition of MCMC start!","\n")
   for(t in 2:iter){
     start_time=Sys.time()
-    
     
     Wg=list()
     
@@ -181,9 +178,9 @@ for(r in 1:R){
     pp_G=1
     
     ### M-H algorithm for sampling Z ###
-    #V=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])+siga_mat[t-1,]*rep(1,mg[g])%*%t(rep(1,mg[g]))
+    
     for(g in 1:G){
-      Z_vec=rep(0,mg[g])
+      Z_vec=zg[[g]]
       Zlist[[g]]=Z_vec
       zz_1=Z_vec
       acc_4v=0
@@ -229,9 +226,9 @@ for(r in 1:R){
             pp=pp*(p_1/p_2)
           }
         }
-        V=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])+siga_mat[t-1,]*rep(1,mg[g])%*%t(rep(1,mg[g]))
+        V=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])
         SS=diag(1,mg[g])-lambda_mat[t-1,]*Wg[[g]]
-        XX=cbind(rep(1,mg[g]),Xg[[g]],Wg[[g]]%*%Xg[[g]])
+        XX=cbind(Xg[[g]],Wg[[g]]%*%Xg[[g]])
         ep=SS%*%(yg[[g]])-XX%*%beta_mat[t-1,]
         like_Y1=exp(-0.5*t(ep-sigez_mat[t-1,]*zz_1)%*%solve(V)%*%(ep-sigez_mat[t-1,]*zz_1))
         like_Y2=exp(-0.5*t(ep-sigez_mat[t-1,]*Zlist[[g]])%*%solve(V)%*%(ep-sigez_mat[t-1,]*Zlist[[g]]))
@@ -276,8 +273,8 @@ for(r in 1:R){
       
       S_1=diag(1,mg[g])-lambda_1*Wg[[g]]
       S_2=diag(1,mg[g])-lambda_mat[t-1,]*Wg[[g]]
-      XX=cbind(rep(1,mg[g]),Xg[[g]],Wg[[g]]%*%Xg[[g]])
-      V=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])+siga_mat[t-1,]*rep(1,mg[g])%*%t(rep(1,mg[g]))
+      XX=cbind(Xg[[g]],Wg[[g]]%*%Xg[[g]])
+      V=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])
       ep_1=S_1%*%yg[[g]]-XX%*%beta_mat[t-1,]
       ep_2=S_2%*%yg[[g]]-XX%*%beta_mat[t-1,]
       like_1=det(S_1)*exp(-0.5*t(ep_1-sigez_mat[t-1,]*Zlist[[g]])%*%solve(V)%*%(ep_1-sigez_mat[t-1,]*Zlist[[g]]))
@@ -317,10 +314,10 @@ for(r in 1:R){
     XVX=matrix(0,ncol=length(beta0),nrow=length(beta0))
     XVY=matrix(0,nrow=length(beta0),ncol=1)
     for(g in 1:G){
-      V=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])+siga_mat[t-1,]*rep(1,mg[g])%*%t(rep(1,mg[g]))
+      V=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])
       SS=diag(1,mg[g])-lambda_mat[t,]*Wg[[g]]
       YY=SS%*%yg[[g]]-sigez_mat[t-1,]*Zlist[[g]]
-      XX=cbind(rep(1,mg[g]),Xg[[g]],Wg[[g]]%*%as.matrix(Xg[[g]]))
+      XX=cbind(Xg[[g]],Wg[[g]]%*%as.matrix(Xg[[g]]))
       XVX=XVX+t(XX)%*%solve(V)%*%XX
       XVY=XVY+t(XX)%*%solve(V)%*%YY
     }
@@ -351,10 +348,10 @@ for(r in 1:R){
 
     pp_sig=1
     for(g in 1:G){
-      V1=(sige_1-sigez_1^2)*diag(1,mg[g])+siga_mat[t-1,]*rep(1,mg[g])%*%t(rep(1,mg[g]))
-      V2=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])+siga_mat[t-1,]*rep(1,mg[g])%*%t(rep(1,mg[g]))
+      V1=(sige_1-sigez_1^2)*diag(1,mg[g])
+      V2=(sige_mat[t-1,]-sigez_mat[t-1,]^2)*diag(1,mg[g])
       SS=diag(1,mg[g])-lambda_mat[t,]*Wg[[g]]
-      XX=cbind(rep(1,mg[g]),Xg[[g]],Wg[[g]]%*%Xg[[g]])
+      XX=cbind(Xg[[g]],Wg[[g]]%*%Xg[[g]])
       ep=SS%*%yg[[g]]-XX%*%beta_mat[t-1,]
       like_1=det(V1)^(-0.5)*exp(-0.5*t(ep-sigez_1*Zlist[[g]])%*%solve(V1)%*%(ep-sigez_1*Zlist[[g]]))
       like_2=det(V2)^(-0.5)*exp(-0.5*t(ep-sigez_mat[t-1,]*Zlist[[g]])%*%solve(V2)%*%(ep-sigez_mat[t-1,]*Zlist[[g]]))
@@ -374,45 +371,39 @@ for(r in 1:R){
     }
     acc_rate3[t,]=acc_3/t
     
+    
     ### sampling of alpha_g from posterior distribution
-    dd=(siga_mat[t-1,]^(-1)+(sige_mat[t,]-sigez_mat[t,]^2)^(-1)*t(rep(1,mg[g]))%*%rep(1,mg[g]))^(-1)
     for(g in 1:G){
+      Rg=(Alpha0^(-1)+(sige_mat[t,]-sigez_mat[t,]^2)^(-1)*t(rep(1,mg[g]))%*%rep(1,mg[g]))^(-1)
       SS=diag(1,mg[g])-lambda_mat[t,]*Wg[[g]]
       YY=SS%*%yg[[g]]-sigez_mat[t,]*Zlist[[g]]
-      XX=cbind(rep(1,mg[g]),Xg[[g]],Wg[[g]]%*%Xg[[g]])
-      XB=XX%*%beta_mat[t,]
-      alpha_mat[t,g]=rnorm(1,(sige_mat[t,]-sigez_mat[t,]^2)^(-1)*dd*(rep(1,mg[g])%*%(YY-XB)),sqrt(dd))
+      XX=cbind(Xg[[g]],Wg[[g]]%*%Xg[[g]])
+      alpha_hat=Rg%*%(Alpha0^(-1)*alpha0+(sige_mat[t,]-sigez_mat[t,]^2)^(-1)*rep(1,mg[g])%*%(YY-XX%*%beta_mat[t,]))
+      alpha_mat[t,g]=rnorm(1,alpha_hat,sqrt(Rg))
     }
-    
-    ### sampling of sigma_alpha from posterior distribution
-    inv_alpha=(rho0+G)/2
-    inv_beta=(eta0+sum((alpha_mat[t,])^2))/2
-    siga_mat[t,]=1/rgamma(1,shape=inv_alpha,rate=inv_beta)
     
     end_time=Sys.time()
     ###print result
-    cat("Constant=",round(beta_mat[t,1],3),"\n")
-    cat("Own Xg =", round(beta_mat[t,2],3),"\n")
-    cat("Own Goal =", round(beta_mat[t,3],3),"\n")
-    cat("Own Shot =", round(beta_mat[t,4],3),"\n")
-    cat("Own Shot Assitst =", round(beta_mat[t,5],3),"\n")
-    cat("Own Cross =", round(beta_mat[t,6],3),"\n")
-    cat("Own Clearance =", round(beta_mat[t,7],3),"\n")
-    cat("Own Dribble =", round(beta_mat[t,8],3),"\n")
-    cat("Own Duel =", round(beta_mat[t,9],3),"\n")
-    cat("Own Pressure =", round(beta_mat[t,10],3),"\n")
-    cat("Peer Xg =", round(beta_mat[t,11],3),"\n")
-    cat("Peer Goal =", round(beta_mat[t,12],3),"\n")
-    cat("Peer Shot =", round(beta_mat[t,13],3),"\n")
-    cat("Peer Shot Assist =", round(beta_mat[t,14],3),"\n")
-    cat("Peer Cross =", round(beta_mat[t,15],3),"\n")
-    cat("Peer Clearance =", round(beta_mat[t,16],3),"\n")
-    cat("Peer Dribble =", round(beta_mat[t,17],3),"\n")
-    cat("Peer Duel =", round(beta_mat[t,18],3),"\n")
-    cat("Peer Pressure =", round(beta_mat[t,19],3),"\n")
+    cat("Own Xg =", round(beta_mat[t,1],3),"\n")
+    cat("Own Goal =", round(beta_mat[t,2],3),"\n")
+    cat("Own Shot =", round(beta_mat[t,3],3),"\n")
+    cat("Own Shot Assitst =", round(beta_mat[t,4],3),"\n")
+    cat("Own Cross =", round(beta_mat[t,5],3),"\n")
+    cat("Own Clearance =", round(beta_mat[t,6],3),"\n")
+    cat("Own Dribble =", round(beta_mat[t,7],3),"\n")
+    cat("Own Duel =", round(beta_mat[t,8],3),"\n")
+    cat("Own Pressure =", round(beta_mat[t,9],3),"\n")
+    cat("Peer Xg =", round(beta_mat[t,10],3),"\n")
+    cat("Peer Goal =", round(beta_mat[t,11],3),"\n")
+    cat("Peer Shot =", round(beta_mat[t,12],3),"\n")
+    cat("Peer Shot Assist =", round(beta_mat[t,13],3),"\n")
+    cat("Peer Cross =", round(beta_mat[t,14],3),"\n")
+    cat("Peer Clearance =", round(beta_mat[t,15],3),"\n")
+    cat("Peer Dribble =", round(beta_mat[t,16],3),"\n")
+    cat("Peer Duel =", round(beta_mat[t,17],3),"\n")
+    cat("Peer Pressure =", round(beta_mat[t,18],3),"\n")
     cat("sigma_e =",round(sige_mat[t,],3),"\n")
     cat("sigma_ez =",round(sigez_mat[t,],3),"\n")
-    cat("siga_alpha =",round(siga_mat[t,],3),"\n")
     cat("gamma acceptance rate=",round(acc_rate1[t,],2),"\n")
     cat("lambda acceptance rate=",round(acc_rate2[t,],2),"\n")
     cat("sigma acceptance rate=",round(acc_rate3[t,],2),"\n")
@@ -423,22 +414,36 @@ for(r in 1:R){
   beta1_list[[r]]=beta_mat[500:iter,1][seq(1,length(beta_mat[500:iter,1]),10)]
   beta2_list[[r]]=beta_mat[500:iter,2][seq(1,length(beta_mat[500:iter,2]),10)]
   beta3_list[[r]]=beta_mat[500:iter,3][seq(1,length(beta_mat[500:iter,3]),10)]
+  beta4_list[[r]]=beta_mat[500:iter,4][seq(1,length(beta_mat[500:iter,4]),10)]
+  beta5_list[[r]]=beta_mat[500:iter,5][seq(1,length(beta_mat[500:iter,5]),10)]
+  beta6_list[[r]]=beta_mat[500:iter,6][seq(1,length(beta_mat[500:iter,6]),10)]
+  beta7_list[[r]]=beta_mat[500:iter,7][seq(1,length(beta_mat[500:iter,7]),10)]
+  beta8_list[[r]]=beta_mat[500:iter,8][seq(1,length(beta_mat[500:iter,8]),10)]
+  beta9_list[[r]]=beta_mat[500:iter,9][seq(1,length(beta_mat[500:iter,9]),10)]
+  beta10_list[[r]]=beta_mat[500:iter,10][seq(1,length(beta_mat[500:iter,10]),10)]
+  beta12_list[[r]]=beta_mat[500:iter,11][seq(1,length(beta_mat[500:iter,11]),10)]
+  beta12_list[[r]]=beta_mat[500:iter,12][seq(1,length(beta_mat[500:iter,12]),10)]
+  beta13_list[[r]]=beta_mat[500:iter,13][seq(1,length(beta_mat[500:iter,13]),10)]
+  beta14_list[[r]]=beta_mat[500:iter,14][seq(1,length(beta_mat[500:iter,14]),10)]
+  beta15_list[[r]]=beta_mat[500:iter,15][seq(1,length(beta_mat[500:iter,15]),10)]
+  beta16_list[[r]]=beta_mat[500:iter,16][seq(1,length(beta_mat[500:iter,16]),10)]
+  beta17_list[[r]]=beta_mat[500:iter,17][seq(1,length(beta_mat[500:iter,17]),10)]
+  beta18_list[[r]]=beta_mat[500:iter,18][seq(1,length(beta_mat[500:iter,18]),10)]
+
   gamma1_list[[r]]=gamma_mat[500:iter,1][seq(1,length(gamma_mat[500:iter,1]),10)]
   gamma2_list[[r]]=gamma_mat[500:iter,2][seq(1,length(gamma_mat[500:iter,2]),10)]
   gamma3_list[[r]]=gamma_mat[500:iter,3][seq(1,length(gamma_mat[500:iter,3]),10)]
+  gamma4_list[[r]]=gamma_mat[500:iter,4][seq(1,length(gamma_mat[500:iter,4]),10)]
+  gamma5_list[[r]]=gamma_mat[500:iter,5][seq(1,length(gamma_mat[500:iter,5]),10)]
+  gamma6_list[[r]]=gamma_mat[500:iter,6][seq(1,length(gamma_mat[500:iter,6]),10)]
+  
   lambda_list[[r]]=lambda_mat[500:iter,][seq(1,length(lambda_mat[500:iter,]),10)]
   sigez_list[[r]]=sigez_mat[500:iter,][seq(1,length(sigez_mat[500:iter,]),10)]
   sige_list[[r]]=sige_mat[500:iter,][seq(1,length(sige_mat[500:iter,]),10)]
-  siga_list[[r]]=siga_mat[500:iter,][seq(1,length(siga_mat[500:iter,]),10)]
   ed_time=Sys.time()
 }
 ed_time=Sys.time()
 cat("total",iter,"iterations spend",ed_time-st_time,"!","\n")
-
-
-
-
-
 
 
 round(mean(beta1_vec),3);round(sd(beta1_vec),3)
